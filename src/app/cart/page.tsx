954931f6fc2101bd4852a6476c3c16142dd00f25
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-09-09 19:51:35 -0300

    feature: cart page aside completed

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,15 +6,96 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
+  font-family: inherit;
 
   section {
     width: 70%;
+
+    h1 {
+      font-size: 2.4rem;
+      font-weight: 500;
+      color: var(--text-dark-2);
+    }
+
+    p {
+      font-size: 1.6rem;
+      font-weight: 300;
+
+      span {
+        font-size: 1.6rem;
+        font-weight: 500;
+      }
+    }
   }
 
   aside {
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 75vh;
+    background-color: var(--shapes-light);
+    padding: 1.2rem 1.6rem;
+    font-size: 1.6rem;
+    font-weight: 400;
+
+    .upper-part {
+      h3 {
+        font-size: 2rem;
+        font-weight: 600;
+        color: var(--text-dark-2);
+      }
+
+      .table {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        justify-content: space-between;
+        margin: 2rem 0;
+
+        > div {
+          margin: 0.4rem 0;
+        }
+
+        .price-total {
+          justify-self: end;
+        }
+      }
+
+      hr {
+        color: var(--shapes);
+      }
+
+      .total-field {
+        font-weight: 600;
+      }
+
+      button {
+        width: 100%;
+        height: 4.4rem;
+        border-radius: 0.4rem;
+        margin-bottom: 0.4rem;
+        font-size: 1.6rem;
+        font-weight: 500;
+        border: none;
+        background-color: var(--green);
+        color: var(--shapes-light);
+        cursor: pointer;
+      }
+    }
+
+    .bottom-part {
+      p {
+        font-size: 1.4rem;
+        font-weight: 500;
+        text-decoration: underline;
+        color: var(--text-dark);
+        cursor: pointer;
+        margin: 0.2rem 0;
+      }
+    }
   }
 `;
 
@@ -28,7 +109,9 @@ export default function Cart() {
         <section>
           <div>
             <h1>SEU CARRINHO</h1>
-            <p>Total ({cart.length} produtos):</p>
+            <p>
+              Total ({cart.length} produtos) <span>0.00</span>
+            </p>
           </div>
           <div>
             {cart.map((item) => (
@@ -36,7 +119,29 @@ export default function Cart() {
             ))}
           </div>
         </section>
-        <aside>resumo</aside>
+        <aside>
+          <div className="upper-part">
+            <h2>RESUMO DO PEDIDO</h2>
+            <div className="table">
+              <div>Subtotal de produtos</div>
+              <div className="price-total">R$ 161,00</div>
+              <div>Entrega</div>
+              <div className="price-total">R$ 40,00</div>
+            </div>
+            <hr />
+            <div className="table total-field">
+              <div>Total</div>
+              <div className="price-total">R$ 40,00</div>
+            </div>
+            <button>FINALIZAR A COMPRA</button>
+          </div>
+          <div className="bottom-part">
+            <p>AJUDA</p>
+            <p>REEMBOLSOS</p>
+            <p>ENTREGAS E FRETE</p>
+            <p>TROCAS E DEVOLUÇÕES</p>
+          </div>
+        </aside>
       </Container>
     </main>
   );
