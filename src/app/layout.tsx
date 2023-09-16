d8338fe12b296d68fbaaafdaf420df0e542ccc39
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-09-16 15:19:12 -0300

    fix: file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Header } from "@/components";
 import type { Metadata } from "next";
 import { Saira } from "next/font/google";
-import { Providers } from "@/providers";
+import { Providers } from "@/Providers";
 
 const saira = Saira({ subsets: ["latin"] });
 
