66f72a1280002187182dbce289539863c3436938
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-09-16 13:46:54 -0300

    refactor: updating cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@
 
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { BackToHomeButton, CartItem, Loading } from "@/components";
+import { BackToHomeButton, CartItem, CartResume, Loading } from "@/components";
 import { useCartWithLocalStorage } from "@/hooks";
 import { LocalStorageProduct } from "@/hooks/useCartWithLocalStorage";
 import { toast } from "react-toastify";
@@ -20,102 +20,31 @@ const Container = styled.div`
     flex-direction: row;
     align-items: flex-start;
   }
+`;
 
-  section {
-    width: 70%;
+const CartItemsSection = styled.section`
+  width: 70%;
 
-    h1 {
-      font-size: 2.4rem;
-      font-weight: 500;
-      color: var(--text-dark-2);
-    }
-
-    p {
-      font-size: 1.6rem;
-      font-weight: 300;
-
-      span {
-        font-size: 1.6rem;
-        font-weight: 500;
-      }
-    }
+  h1 {
+    font-size: 2.4rem;
+    font-weight: 500;
+    color: var(--text-dark-2);
   }
 
-  aside {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    min-height: 75vh;
-    background-color: var(--shapes-light);
-    padding: 1.2rem 1.6rem;
+  p {
     font-size: 1.6rem;
-    font-weight: 400;
+    font-weight: 300;
 
-    .upper-part {
-      h3 {
-        font-size: 2rem;
-        font-weight: 600;
-        color: var(--text-dark-2);
-      }
-
-      .table {
-        width: 100%;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        justify-content: space-between;
-        margin: 2rem 0;
-
-        > div {
-          margin: 0.4rem 0;
-        }
-
-        .price-total {
-          justify-self: end;
-        }
-      }
-
-      hr {
-        color: var(--shapes);
-      }
-
-      .total-field {
-        font-weight: 600;
-      }
-
-      button {
-        width: 100%;
-        height: 4.4rem;
-        border-radius: 0.4rem;
-        margin-bottom: 0.4rem;
-        font-size: 1.6rem;
-        font-weight: 500;
-        border: none;
-        background-color: var(--green);
-        color: var(--shapes-light);
-        cursor: pointer;
-
-        &:hover {
-          opacity: 0.9;
-        }
-      }
-    }
-
-    .bottom-part {
-      p {
-        font-size: 1.4rem;
-        font-weight: 500;
-        text-decoration: underline;
-        color: var(--text-dark);
-        cursor: pointer;
-        margin: 0.2rem 0;
-      }
+    span {
+      font-size: 1.6rem;
+      font-weight: 500;
     }
   }
 `;
 
 export default function Cart() {
-  const { cart, removeFromCart } = useCartWithLocalStorage();
+  const { cart, updateLocalStorage, clearCartFromlocalStorage } =
+    useCartWithLocalStorage();
   const [cartItems, setCartItems] = useState<LocalStorageProduct[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -130,9 +59,9 @@ export default function Cart() {
   }, [cart]);
 
   const handleRemoveFromCart = (id: number) => {
-    removeFromCart(id);
     const newCartItems = cartItems.filter((item) => item.id !== id);
     setCartItems(newCartItems);
+    updateLocalStorage(newCartItems);
   };
 
   const handleFinishCart = (): void => {
@@ -140,28 +69,37 @@ export default function Cart() {
       autoClose: false,
       position: "top-center",
     });
+    setCartItems([]);
+    clearCartFromlocalStorage();
   };
 
-  const cartItemsPrice = cartItems.reduce(
-    (price, item) => item.price_in_cents + price,
+  const updateCartQuantity = (id: number, quantity: number) => {
+    const newValue = cartItems.map((item) => {
+      if (item.id !== id) return item;
+      return { ...item, quantity };
+    });
+    updateLocalStorage(newValue);
+  };
+
+  const deliveryFee = 4000;
+
+  const cartItemsTotalPrice = cartItems.reduce(
+    (sum, item) => (sum += item.price_in_cents * item.quantity),
     0
   );
 
-  const cartItemsTotalPrice = cartItems.reduce(
-    (total, item) => item.price_in_cents * item.quantity + total,
-    0
-  );
+  const totalPriceWDeliveryFee = formatPrice(cartItemsTotalPrice + deliveryFee);
 
   return (
     <main>
       <BackToHomeButton />
       <Container>
-        <section>
+        <CartItemsSection>
           <div>
             <h1>SEU CARRINHO</h1>
             <p>
               Total ({cartItems.length} produtos){" "}
-              <span>{formatPrice(cartItemsPrice)}</span>
+              <span>{formatPrice(cartItemsTotalPrice)}</span>
             </p>
           </div>
           <ul>
@@ -171,35 +109,17 @@ export default function Cart() {
                 <CartItem
                   {...product}
                   handleRemoveFromCart={handleRemoveFromCart}
+                  updateCartQuantity={updateCartQuantity}
                 />
               ))}
           </ul>
-        </section>
-        <aside>
-          <div className="upper-part">
-            <h2>RESUMO DO PEDIDO</h2>
-            <div className="table">
-              <div>Subtotal de produtos</div>
-              <div className="price-total">
-                {formatPrice(cartItemsTotalPrice)}
-              </div>
-              <div>Entrega</div>
-              <div className="price-total">R$ 40,00</div>
-            </div>
-            <hr />
-            <div className="table total-field">
-              <div>Total</div>
-              <div className="price-total">R$ 40,00</div>
-            </div>
-            <button onClick={handleFinishCart}>FINALIZAR A COMPRA</button>
-          </div>
-          <div className="bottom-part">
-            <p>AJUDA</p>
-            <p>REEMBOLSOS</p>
-            <p>ENTREGAS E FRETE</p>
-            <p>TROCAS E DEVOLUÇÕES</p>
-          </div>
-        </aside>
+        </CartItemsSection>
+        <CartResume
+          totalItemsPrice={formatPrice(cartItemsTotalPrice)}
+          deliveryFee={formatPrice(deliveryFee)}
+          handleFinishCart={handleFinishCart}
+          totalPriceWDeliveryFee={totalPriceWDeliveryFee}
+        />
       </Container>
     </main>
   );
